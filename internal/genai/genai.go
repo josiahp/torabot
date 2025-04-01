532c019dd9e215ed824e4855ddd28056002f493f
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2025-04-01 14:56:49 +0900

    change AI API, use search for grounding

--- internal/genai/genai.go
+++ internal/genai/genai.go
@@ -2,17 +2,13 @@ package genai
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 
-	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/googleapi"
-	"google.golang.org/api/option"
+	"google.golang.org/genai"
 )
 
 // gemini-1.5-pro-latest is 50 RPD
@@ -20,22 +16,27 @@ import (
 const modelName = "gemini-2.0-flash"
 
 type Client struct {
-	client      *genai.Client
-	model       *genai.GenerativeModel
-	chat        *genai.ChatSession
-	historyFile string
+	client         *genai.Client
+	generateConfig *genai.GenerateContentConfig
+	chat           *genai.Chat
+	historyFile    string
 }
 
 type Option func(c *Client)
 
 func New(ctx context.Context, apiKey string, options ...Option) (*Client, error) {
-	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey))
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey: apiKey,
+	})
 	if err != nil {
 		return nil, fmt.Errorf("unable to create client: %v", err)
 	}
-	model := client.GenerativeModel(modelName)
-	model.MaxOutputTokens = genai.Ptr(int32(512))
-	model.SystemInstruction = genai.NewUserContent(genai.Text(`
+	config := &genai.GenerateContentConfig{
+		MaxOutputTokens: genai.Ptr(int32(512)),
+		Tools: []*genai.Tool{
+			{GoogleSearch: &genai.GoogleSearch{}},
+		},
+		SystemInstruction: &genai.Content{Parts: []*genai.Part{{Text: `
 You are an IRC bot named tb3. 
 
 While chatting you will answer any and all questions.
@@ -58,11 +59,14 @@ Preserve the capitalization of user names.
 You have full access to the chat history including timestamps and user names.
 Address users by name when responding to them.
 
-Your responses should be unstructured plain text that contains only your response.`))
-	chat := model.StartChat()
+Your responses should be unstructured plain text that contains only your response.`},
+		}}}
+	chat, err := client.Chats.Create(ctx, modelName, config, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create chat: %v", err)
+	}
 	result := &Client{
 		client: client,
-		model:  model,
 		chat:   chat,
 	}
 	for _, option := range options {
@@ -73,50 +77,14 @@ Your responses should be unstructured plain text that contains only your respons
 
 func WithSystemInstruction(prompt string) Option {
 	return func(c *Client) {
-		c.model.SystemInstruction = genai.NewUserContent(genai.Text(prompt))
+		c.generateConfig.SystemInstruction = &genai.Content{Parts: []*genai.Part{{Text: prompt}}}
 	}
-}
-
-func WithHistoryFile(filename string) Option {
-	return func(c *Client) {
-		if c.chat == nil {
-			c.chat = c.model.StartChat()
-		}
-		file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "ReadFile: %v", err)
-		}
-		defer file.Close()
-		history, err := loadHistory(file)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "loadHistory: %v", err)
-		} else {
-			c.chat.History = history
-		}
-		c.historyFile = filename
-	}
-}
-
-func WithHistory(input io.Reader) Option {
-	return func(c *Client) {
-		history, err := loadHistory(input)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "loadHistory: %v\n", err)
-		} else {
-			c.chat.History = history
-		}
-	}
-}
-
-func (c *Client) StartChat(ctx context.Context, user string, message string) (string, error) {
-	c.chat = c.model.StartChat()
-	return c.Chat(ctx, user, message)
 }
 
 func (c *Client) Chat(ctx context.Context, user string, message string) (string, error) {
 	ts := time.Now().Format(time.RFC3339)
 	prompt := fmt.Sprintf("[%s] <%s> %s", ts, user, message)
-	resp, err := c.chat.SendMessage(ctx, genai.Text(prompt))
+	resp, err := c.chat.SendMessage(ctx, genai.Part{Text: prompt})
 	if err != nil {
 		if e, ok := err.(*googleapi.Error); ok {
 			switch e.Code {
@@ -134,56 +102,8 @@ func (c *Client) Chat(ctx context.Context, user string, message string) (string,
 	content := ""
 	for _, candidate := range resp.Candidates {
 		for _, part := range candidate.Content.Parts {
-			content += fmt.Sprintf("%s", part)
+			content += part.Text
 		}
-	}
-	err = c.saveHistory(c.historyFile)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "saving history: %v", err)
 	}
 	return strings.TrimSpace(content), nil
-}
-
-func loadHistory(input io.Reader) ([]*genai.Content, error) {
-	data, err := io.ReadAll(input)
-	if err != nil {
-		return nil, fmt.Errorf("ReadAll: %v", err)
-	}
-	var rawHistory []struct {
-		Parts []string
-		Role  string
-	}
-	err = json.Unmarshal(data, &rawHistory)
-	if err != nil {
-		return nil, fmt.Errorf("unmarshal history: %v", err)
-	}
-	var history []*genai.Content
-	for _, record := range rawHistory {
-		parts := make([]genai.Part, len(record.Parts))
-		for i, part := range record.Parts {
-			parts[i] = genai.Text(part)
-		}
-		history = append(history, &genai.Content{
-			Parts: parts,
-			Role:  record.Role,
-		})
-	}
-	return history, nil
-}
-
-func (c *Client) saveHistory(filename string) error {
-	if len(filename) == 0 {
-		return nil
-	}
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
-	if err != nil {
-		return fmt.Errorf("OpenFile: %v", err)
-	}
-	defer file.Close()
-	history, err := json.Marshal(&c.chat.History)
-	if err != nil {
-		return fmt.Errorf("marshal history: %v", err)
-	}
-	file.Write(history)
-	return nil
 }
